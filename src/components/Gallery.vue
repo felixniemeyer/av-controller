09634bd06aa1228324947278279fb947682017de
Felix Niemeyer <niemeyer.felix@gmail.com>
2024-04-09 09:37:46 +0200

    implement GalleryEntry, enter 3 artworks

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { defineEmits, ref, onMounted } from 'vue'
 
+import GalleryEntryComponent from './GalleryEntry.vue'
+
 const visualTabUrl = ref('')
 const urlInputField = ref(null as HTMLInputElement | null)
 
@@ -14,13 +16,7 @@ onMounted(() => {
 
 function confirmOnEnter(e: KeyboardEvent, url?: string) {
   if(e.key === 'Enter' || e.key === ' ') {
-    if(url) {
-      openExample(url)
-    } else {
-      openVisualsTab()
-    }
-    e.stopPropagation()
-    e.preventDefault()
+    openVisualsTab()
   }
 }
 
@@ -33,65 +29,105 @@ function openVisualsTab() {
   emits('openVisualsTab', url)
 }
 
-const examples = import.meta.env.DEV ? {
-  'localhost:5173': 'http://localhost:5173',
-} : {
-  'aortic rupture': 'https://gfx.aimparency.org/rupture/',
-  'music-box': 'https://gfx.aimparency.org/music-box-song/'
-} as Record<string, string>
+export interface Entry {
+  artist: {
+    alias: string
+    profileUrl?: string
+  }, 
+  artwork: {
+    title: string
+    subtitle?: string
+    description?: string
+    url: string
+    nftUrl?: string
+  }
+  screenshotUrl: string
+}
+
+const forceProd = false
+const entries = (!forceProd && import.meta.env.DEV) ? [5173, 5174, 5175, 3000, 8000, 8080].map(port =>
+  ({
+    artist: {
+      alias: 'myself', 
+    }, 
+    artwork: {
+      title: `localhost:${port}`, 
+      url: `http://localhost:${port}`,
+    }, 
+    screenshotUrl: './localhost.svg',
+  })
+) : [
+  {
+    artist: {
+      alias: 'fairlix', 
+      profileUrl: 'https://linktr.ee/fairlix', 
+    },
+    artwork: {
+      title: 'music box', 
+      description: 'Balls in a square make sounds when colliding with the walls. Controls allow to change musical elements like the chord progression and bass pattern.', 
+      url: 'https://gfx.aimparency.org/music-box-nft/',
+      nftUrl: 'https://collector.sh/fairlix/musicbox',
+    },
+    screenshotUrl: './musicbox.png',
+  },
+  {
+    artist: {
+      alias: 'fairlix',
+      profileUrl: 'https://linktr.ee/fairlix', 
+    },
+    artwork: {
+      title: 'aortic rupture', 
+      subtitle: 'raymarching extruded ant particle paths', 
+      description: 'The white structure in the foreground is the "pheromone map" of ant particles. Using blur and log a 2D distance map is created from that, which is extruded and twisted into the 3D object (aorta) in the background.', 
+      url: 'https://gfx.aimparency.org/rupture/',
+    },
+    screenshotUrl: './rupture.png',
+  },
+  {
+    artist: {
+      alias: 'fairlix', 
+      profileUrl: 'https://linktr.ee/fairlix', 
+    },
+    artwork: {
+      title: 'spacies', 
+      subtitle: 'cities in space',
+      description: 'generative 3d objects and colorful shaders', 
+      url: 'https://gfx.aimparency.org/rupture/spacies-av-controlled/',
+      nftUrl: 'https://opensea.io/collection/spacies', 
+    },
+    screenshotUrl: './spacies.png',
+  },
+] as Entry[]
 
 </script>
 
 <template>
   <div>
-    <h1>av control</h1>
-    <p>
-      Open a new tab with a av-control receiving webapp
+    <h1>av controller gallery</h1>
+    
+    <p> 
+      Enter any URL of a web page with an artwork that exposes 
+      <a href='https://github.com/felixniemeyer/av-controls' target="_blank">av controls</a>
+      manually: 
     </p>
     <input type="text" ref='urlInputField' v-model="visualTabUrl" @keydown=confirmOnEnter placeholder="URL"/>
-    <button @click="openVisualsTab">open</button>
+    <button @click="openVisualsTab">open in separate tab</button>
+    <p>
+      Or click a screenshot to open the artwork in a separate tab. <br>
+    </p>
+    <p>
+      The gallery tab will then display the controls. 
+    </p>
     <div class=examples >
-      <div v-for="(url, name) in examples" 
-        class=exwrap 
-        @click="openExample(url)" 
-        @keydown="confirmOnEnter($event, url)"
-        tabindex="0" >
-        <div class=example>
-          {{ name }}
-        </div>
-      </div>
+      <GalleryEntryComponent
+         v-for="entry in entries" 
+         :entry="entry"
+         @openTab="openExample(entry.artwork.url)" 
+         />
     </div>
   </div>
 </template>
 
 <style scoped>
-
-.exwrap {
-  display: inline-block;
-  margin: 0.5rem; 
-  width: 2rem; 
-  height: 20rem;
-  background-color: #fff3;
-  cursor: pointer;
-  border-radius: 0.5rem;
-}
-
-.exwrap:hover {
-  background-color: #fff6;
-}
-
-.example {
-  width: 2rem; 
-  height: 2rem; 
-  overflow: visible;
-  white-space: nowrap;
-  position: relative;
-  display: inline-block;
-  transform-origin: 50% 50%;
-  transform: rotate(90deg);
-  pointer-events: none;
-  line-height: 2rem;
-  padding: 0 0.5rem;
-}
 
 </style>
---
+++ src/components/GalleryEntry.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { type Entry } from './Gallery.vue'
+
+const props = defineProps<{
+  entry: Entry
+}>()
+
+const emits = defineEmits(['openTab'])
+
+function confirmOnEnter(e: KeyboardEvent) {
+  if(e.key === 'Enter' || e.key === ' ') {
+    emits('openTab')
+    e.stopPropagation()
+    e.preventDefault()
+  }
+}
+
+// computed
+const linkName = computed(() => {
+  return new URL(props.entry.artist.profileUrl!).hostname
+})
+
+const nftLinkName = computed(() => {
+  return new URL(props.entry.artwork.nftUrl!).hostname
+})
+
+</script>
+
+<template>
+  <div class="entry">
+    <div class="page info">
+      <h2>
+        {{ props.entry.artwork.title }}
+      </h2>
+      <p> 
+        by {{ props.entry.artist.alias }}
+        <template v-if="props.entry.artist.profileUrl">
+          (
+          <a v-if="props.entry.artist.profileUrl" :href="props.entry.artist.profileUrl">
+            {{ linkName }}
+          </a>
+          )
+        </template>
+      </p>
+      <h3 v-if="props.entry.artwork.subtitle">
+        {{ props.entry.artwork.subtitle }}
+      </h3>
+      <p v-if="props.entry.artwork.description">
+        {{ props.entry.artwork.description }}
+      </p>
+      <p v-if="props.entry.artwork.nftUrl">
+        NFTs on 
+        <a :href="props.entry.artwork.nftUrl">
+          {{ nftLinkName }}
+        </a>
+      </p>
+    </div>
+    <div 
+      tabindex=0 
+      @keydown=confirmOnEnter 
+      @click="emits('openTab')"
+      class="page screenshot">
+      <img v-if="props.entry.screenshotUrl" :src="props.entry.screenshotUrl" />
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+.entry {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 2rem;
+}
+
+.info {
+  width: 40%;
+  margin: 2rem; 
+  text-align: right;
+}
+
+.screenshot {
+  text-align: left;
+  max-width: 40%;
+  min-width: 4rem; 
+  min-height: 4rem;
+  cursor: pointer;
+}
+
+.screenshot:focus {
+  outline: 1px solid #fff8;
+}
+.screenshot img {
+  max-width: 100%;
+  max-height: 100%;
+  pointer-events: none;
+}
+
+</style>
